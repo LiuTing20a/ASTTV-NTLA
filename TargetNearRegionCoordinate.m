c95e1e008305cd1935e3902b03cdb0f458614167
LiuTing20a <93706389+LiuTing20a@users.noreply.github.com>
2021-11-11 11:21:20 +0800

    Delete TargetNearRegionCoordinate.m

--- TargetNearRegionCoordinate.m
+++
@@ -1,9 +0,0 @@
-function [Coordinates_x, Coordinates_y]= TargetNearRegionCoordinate(x, y, Diameter)
-%计算目标临近区域坐标
-%目标坐标[x,y]
-%目标邻域大小Diameter,奇数
-Radius = (Diameter-1)/2;
-Coordinates_x = [1:Diameter]-Radius-1 +x;
-Coordinates_y = [1:Diameter]-Radius-1 +y;
-end
-
